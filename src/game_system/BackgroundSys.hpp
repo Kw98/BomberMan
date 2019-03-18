9001b2e606fe8117baf2edc625fd985e90ab124d
victor <victor.zhu@epitech.eu>
2019-03-18 12:27:07 +0100

    [MAP] background ok with file (and default)

--- src/game_system/BackgroundSys.hpp
+++ src/game_system/BackgroundSys.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <fstream>
+#include <iostream>
+#include <ctime>
 #include "Components.hpp"
 
 namespace Bomber {
@@ -8,30 +11,32 @@ namespace Bomber {
 	{
 	public:
 		BackgroundSys(gloop::GLoop &gloop) {
+			std::srand(std::time(nullptr));
 			gloop::StageManager	&stages = gloop.get_stage_manager();
-
 			gloop::SystemHook hook = gloop::SystemHook{"BACKGROUND_SYS", 10, 0, false, [this](GEcm::Register &reg, gloop::GLoop &) {
-				for (int y = -30; y < Bomber::MAX_Y + 30; y += 5) {
-					for (int x = -30; x < Bomber::MAX_X + 30; x++) {
-						if ((x >= 0 && x <= Bomber::MAX_X) &&
-							(y > 0 && y < Bomber::MAX_Y)) {
-						} else {
-							auto ground = reg.create();
-							reg.construct<Bomber::Graphics>(ground, true, Bomber::BACKGROUND_T, Bomber::ItemType::CUBE, Bomber::State::NEW, 5.0);
-							reg.construct<Bomber::Pos>(ground, x, y, -6);
-							x += 4;
+				std::ifstream myfile ("assets/backgroundMap.txt");
+				std::string line;
+				if (myfile.is_open()) {
+					int y = -10;
+					 while (getline(myfile,line)) {
+						int x = -25;
+					 	for (auto &&block: line) {
+					 		setBlock(reg, x, y, int(block) - '0');
+					 		x += 1;
+					 	}
+					 	y += 5;
+					 }
+					myfile.close();
+				}
+				else {
+					std::cout << "Unable to open file" << std::endl;;
+					for (int y = -30; y < Bomber::MAX_Y + 30; y += 5) {
+						for (int x = -30; x < Bomber::MAX_X + 30; x++) {
+							int block = std::rand() % 4;
+							setBlock(reg, x, y, block);
 						}
 					}
 				}
-				auto titleImg = reg.create();
-				reg.construct<Bomber::Graphics>(titleImg, true, Bomber::TITLE_T, Bomber::ItemType::MESH, Bomber::State::NEW, 1.0);
-				reg.construct<Bomber::Pos>(titleImg, 25, 30, 6);
-				// ITexture* images = _driver->getTexture(Bomber::TITLE_T);
-
-				// _driver->draw2DImage(images, core::position2d<s32>(25, 30),
-				// 	core::rect<s32>(0,0,256,72), 0,
-				// 	video::SColor(255,255,255,255), true);
-
 				return gloop::HookStatus::OK; }};
 
 			stages.get_stage(gloop::StageType::INIT).add_hook(gloop::HookType::EARLY, hook);
@@ -39,6 +44,21 @@ namespace Bomber {
 
 		~BackgroundSys() {
 
-		};	
+		};
+
+	private:
+		void	setBlock(GEcm::Register &reg, int &x, int y, int block) {
+			if ((x >= 0 && x <= Bomber::MAX_X) &&
+				(y > 0 && y < Bomber::MAX_Y)) {
+			} else {
+				auto ground = reg.create();
+				if (block == 1)
+					reg.construct<Bomber::Graphics>(ground, true, Bomber::BACKGROUND_GRASS_T, Bomber::ItemType::CUBE, Bomber::State::NEW, 5.0);
+				else
+					reg.construct<Bomber::Graphics>(ground, true, Bomber::BACKGROUND_T, Bomber::ItemType::CUBE, Bomber::State::NEW, 5.0);
+				reg.construct<Bomber::Pos>(ground, x, y, -6);
+			}
+			x += 4;
+		}
 	};
 } // Bomber
